822455246635a669b69225cd5d92779b74bd9b6b
AssureOps <graham.atkinson@assureops.uk>
2025-10-06 09:33:00 +0100

    project view2

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 import { OrgProvider } from "./OrgContext";
@@ -14,13 +13,17 @@ import OrgUsers from "./OrgUsers";
 import Templates from "./Templates";
 import AllocateCriteria from "./AllocateCriteria";
 import Projects from "./Projects"; // tile view
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <OrgProvider>
       <BrowserRouter>
         <Routes>
-          <Route element={<Layout />}>
+<Route element={<Layout />}>
+  {/* ✅ when visiting "/" */}
+  <Route index element={<Navigate to="/projects" replace />} />
+
   {/* Global pages */}
   <Route path="/projects" element={<Projects />} />
   <Route path="/templates" element={<Templates />} />
@@ -28,11 +31,14 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 
   {/* Project-scoped pages */}
   <Route path="/projects/:id" element={<ProjectLayout />}>
-    <Route index element={<App />} />                 {/* ✅ Checklist */}
+    <Route index element={<App />} />
     <Route path="dashboard" element={<Dashboard />} />
     <Route path="allocate" element={<AllocateCriteria />} />
     <Route path="settings" element={<ProjectPage />} />
   </Route>
+
+  {/* ✅ any other unknown path */}
+  <Route path="*" element={<Navigate to="/projects" replace />} />
 </Route>
 
         </Routes>
